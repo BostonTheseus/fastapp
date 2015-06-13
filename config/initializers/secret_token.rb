8debace6706b968bca9340fbf1d7baad7a7f5c1b
Robert Morris <obviouslyrob@gmail.com>
2015-06-13 23:37:59 +0000

    Initial commit for fastapp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fastapp::Application.config.secret_key_base = '31b99dc1d34e18dd27fc7c5af6a67ad0c78825ffe5ca2e17162c4033df5b6e12a966c93d47a67223fe66274bddd04ca40332fd9e321202e8ecd8016787801c05'
